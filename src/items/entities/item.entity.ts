b05c2486de7267f0c765dff4c15d56e682fb17ca
shohan-ontik <shohan@ontik.technology>
2026-03-04 00:24:01 +0600

    One to many

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,17 +1,10 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { AbstractEntity } from 'src/database/abstract.entity';
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
+import { Comment } from './comment.entity';
 import { Listing } from './listing.entity';
 
 @Entity()
-export class Item {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class Item extends AbstractEntity<Item> {
   @Column()
   name: string;
 
@@ -22,7 +15,6 @@ export class Item {
   @JoinColumn()
   listing: Listing;
 
-  constructor(item: Partial<Item>) {
-    Object.assign(this, item);
-  }
+  @OneToMany(() => Comment, (comment) => comment.item, { cascade: true })
+  comments: Comment[];
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { Comment } from './entities/comment.entity';
 import { Item } from './entities/item.entity';
 import { Listing } from './entities/listing.entity';
 
@@ -12,14 +13,20 @@ export class ItemsService {
     @InjectRepository(Item) private readonly itemRepository: Repository<Item>,
     private readonly entityManager: EntityManager,
   ) {}
+
   async create(createItemDto: CreateItemDto) {
+    console.log('>>>>>>>>>> ', createItemDto);
     const listing = new Listing({
       ...createItemDto.listing,
       rating: 0,
     });
-    const item = new Item({ ...createItemDto, listing });
+
+    const item = new Item({
+      ...createItemDto,
+      comments: [],
+      listing,
+    });
     await this.entityManager.save(item);
-    return 'This action adds a new item';
   }
 
   async findAll() {
@@ -30,7 +37,7 @@ export class ItemsService {
   async findOne(id: number) {
     return this.itemRepository.findOne({
       where: { id },
-      relations: { listing: true },
+      relations: { listing: true, comments: true },
     });
   }
 
@@ -38,7 +45,12 @@ export class ItemsService {
     const item = await this.itemRepository.findOneBy({ id });
     if (item) {
       item.public = updateItemDto.public;
+      const comments = updateItemDto.comments.map(
+        (createCommentDto) => new Comment(createCommentDto),
+      );
+      item.comments = comments;
       await this.entityManager.save(item);
+
       return `This action updates a #${id} item`;
     }
   }
